b25eb297c2439da6442c93b1471c64699ce17550
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2022-01-14 22:38:07 +0100

    Revert to snapshot testing instead of visual testing

--- libs/ui/src/storyshot.test.js
+++ libs/ui/src/storyshot.test.js
@@ -1,6 +1,3 @@
 import initStoryshots from '@storybook/addon-storyshots';
-import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
-initStoryshots({
-  test: imageSnapshot({ storybookUrl: 'http://localhost:4400/' }),
-});
+
 initStoryshots();
