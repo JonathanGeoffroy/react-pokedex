6b85ca71d41861fc8297f0ef64180d80fbf3f685
Jonathan Geoffroy <geoffroy.jonathan@gmail.com>
2022-03-01 17:52:16 +0100

    style: don't crop siblings for computer screens

--- apps/web/src/components/pokemon/details/pokemon-details.tsx
+++ apps/web/src/components/pokemon/details/pokemon-details.tsx
@@ -67,7 +67,7 @@ export function PokemonDetails(props: PokemonDetailsProps) {
         </div>
       </div>
 
-      <div className="flex flex-row justify-between items-end -mx-8">
+      <div className="flex flex-row justify-between items-end -mx-8 sm:mx-0">
         <SiblingPokemon pokemon={pokemon?.previous} />
         <div className="self-center relative top-12 z-10 h-48 w-48">
           {pokemon ? (
